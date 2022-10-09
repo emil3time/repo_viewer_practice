b482b3e77f05b2c40c97ac9e1994d60b1d9b9c0a
Emil <ezmil34@gmail.com>
2022-10-09 22:47:16 +0200

    feat: auth

---
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -0,0 +1,122 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:oauth2/oauth2.dart';
+import 'package:repo_viewer/auth/domain/auth_failure.dart';
+import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
+
+import 'package:repo_viewer/core/shared/encoders.dart';
+
+class GithubOAuthHttpClient extends http.BaseClient {
+  final httpClient = http.Client();
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Accept'] = 'application/json';
+
+    return httpClient.send(request);
+  }
+}
+
+class GithubAuthenticator {
+  final CredentialsStorage _credentialsStorage;
+  final Dio _dio;
+
+  GithubAuthenticator(this._credentialsStorage, this._dio);
+//TOdo deal witch clientSecret
+  static const clientId = 'xxxxxxxxx';
+  static const clientSecret = 'xxxxxxxx';
+  static const scopes = ['read:user', 'repo'];
+  static final authorizationEndpoint =
+      Uri.parse('https://github.com/login/oauth/authorize');
+  static final tokenEndpoint =
+      Uri.parse('https://github.com/login/oauth/access_token');
+  static final redirectUrl = Uri.parse('http://localhost:3000/callback');
+  static final revokedEndpoint =
+      Uri.parse('https://api.github.com/applications/$clientId/token');
+
+  Future<Credentials?> getSignedInCredentials() async {
+    try {
+      final storedCredentials = await _credentialsStorage.read();
+
+      if (storedCredentials != null) {
+        if (storedCredentials.canRefresh && storedCredentials.isExpired) {
+          //TODO refresh
+        }
+      }
+
+      return storedCredentials;
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignIn() =>
+      getSignedInCredentials().then((credentials) => credentials != null);
+
+  AuthorizationCodeGrant createGrant() {
+    return AuthorizationCodeGrant(
+      clientId,
+      authorizationEndpoint,
+      tokenEndpoint,
+      secret: clientSecret,
+      httpClient: GithubOAuthHttpClient(),
+    );
+  }
+
+  Uri getAuthorizationUrl(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectUrl, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Unit>> handleAuthorization(
+    AuthorizationCodeGrant grant,
+    Map<String, String> queryParams,
+  ) async {
+    try {
+      final httpClient = await grant.handleAuthorizationResponse(queryParams);
+
+      await _credentialsStorage.save(httpClient.credentials);
+
+      return right(unit);
+    } on FormatException {
+      return left(const AuthFailure.server());
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server('${e.error} : ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Unit>> signOut() async {
+    final accessToken = _credentialsStorage
+        .read()
+        .then((credentials) => credentials?.accessToken);
+
+    final clientIdAndSecret = stringToBase64.encode('$clientId:$clientSecret');
+
+    try {
+      try {
+        _dio.deleteUri(
+          revokedEndpoint,
+          data: {'access_token': accessToken},
+          options: Options(
+            headers: {'Authorization': 'basic $clientIdAndSecret'},
+          ),
+        );
+      } on DioError catch (e) {
+        if (e.isNoInternetConnection) {
+          //ignoring
+        } else {
+          rethrow;
+        }
+      }
+
+      await _credentialsStorage.clear();
+      return right(unit);
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+}
---
+++ lib/core/shared/encoders.dart
@@ -0,0 +1,4 @@
+import 'dart:convert';
+
+//base64.encode(utf8.encode('string'))
+final stringToBase64 =  utf8.fuse(base64);
