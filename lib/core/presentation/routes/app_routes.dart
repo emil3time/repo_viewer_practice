74057c30cd05878acd4a777a66efb8c6844e674d
Emil <ezmil34@gmail.com>
2022-10-12 12:25:53 +0200

    faet: auth || Navigator 2.0 witch auto_route.

---
+++ lib/core/presentation/routes/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:repo_viewer/auth/presentation/sign_in_page.dart';
+import 'package:repo_viewer/splash/splash_page.dart';
+import 'package:repo_viewer/starred_repo/starred_repo_page.dart';
+
+@MaterialAutoRouter(
+  routes: [
+    MaterialRoute(page: SplashPage, initial: true),
+    MaterialRoute(page: SignInPage, path: '/sign-in' ),
+    MaterialRoute(page: StarredRepoPage,path:  '/starred'),
+  ],
+  replaceInRouteName: 'Page,Route',
+)
+class $AppRouter {}
---
+++ lib/starred_repo/starred_repo_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class StarredRepoPage extends StatelessWidget {
+  const StarredRepoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
